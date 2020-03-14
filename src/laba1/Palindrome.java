e9663bca3a5be80d824b561ce1aaa00be918ca80
Artyom333 <artyom458787@gmail.com>
2020-03-14 23:12:06 +0300

    laba1

---
+++ src/laba1/Palindrome.java
@@ -0,0 +1,26 @@
+package laba1;
+import java.util.Scanner;
+
+public class Palindrome{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        String s=in.next();
+        System.out.println(isPalindrome(s));
+    }
+    public static String reverseString(String s){
+        String str="";
+
+        for(int i=s.length();i>0;i--){
+            str+=s.charAt(i-1);
+        }
+        return str;
+    }
+    public static boolean isPalindrome(String s){
+
+        if(s.equals(reverseString(s)))
+            return true;
+        else
+            return false;
+    }
+
+}
