24855fd0cf7d2e19d733d1ca17beb5c13592317f
artyom <artyom458787@gamil.com>
2020-05-15 22:28:25 +0300

    laba4

--- src/laba1/Primes.java
+++ src/laba1/Primes.java
@@ -2,9 +2,9 @@ package laba1;
 
 public class Primes {
     public static void main(String[] args) {
-        for(int i=2;i<=100;i++){
-            if(isPrime(i)){
-                System.out.println(i);
+        for(int i=2;i<=100;i++){  //создаем цикл от 2до 100
+            if(isPrime(i)){            //вызываем метод isPrime с параметром i
+                System.out.println(i);    //вывод
             }
         }
 
@@ -14,7 +14,7 @@ public class Primes {
     public static boolean isPrime(int n) {
         int m=0;
         for(int inter=1;inter<=n;inter++){
-            if (n%inter==0) m+=1;
+            if (n%inter==0) m+=1;       // если n делится с отстатком на inter прибавлям к m 1
 
         }
        if(m<=2)return true;
